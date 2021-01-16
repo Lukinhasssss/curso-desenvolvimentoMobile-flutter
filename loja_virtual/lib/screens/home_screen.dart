5b594f6f41fd3a5adfd2b131f19b60d43b6c4ecb
Lucas Monteiro <lukasmonteir0@hotmail.com>
2021-01-16 08:46:40 -0300

    Loja Virtual: Creating a custom drawer

--- loja_virtual/lib/screens/home_screen.dart
+++ loja_virtual/lib/screens/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+
 import 'package:loja_virtual/tabs/home_tab.dart';
+import 'package:loja_virtual/widgets/custom_drawer.dart';
 
 class HomeScreen extends StatelessWidget {
 
@@ -11,7 +13,10 @@ class HomeScreen extends StatelessWidget {
       controller: _pageController,
       physics: NeverScrollableScrollPhysics(), // Desta forma é possível controlar o scroll a partir de um código. Para isso é preciso setar um controlador
       children: <Widget>[
-        HomeTab()
+        Scaffold(
+          body: HomeTab(),
+          drawer: CustomDrawer(),
+        )
       ], // children -> Receba vários widgets
     );
   }
