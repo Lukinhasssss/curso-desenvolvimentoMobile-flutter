4b0df73aa7828b4d4e34788a1470aa0ee579899b
Lucas Monteiro <lukasmonteir0@hotmail.com>
2021-01-18 21:44:21 -0300

    Loja Virtual: Add login screen

---
+++ loja_virtual/lib/screens/login_screen.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Entrar'),
+        centerTitle: true,
+        actions: <Widget>[
+          FlatButton(
+            child: Text('CRIAR CONTA', style: TextStyle(fontSize: 15.0)),
+            textColor: Colors.white,
+            onPressed: () {
+              print('CRIAR CONTA');
+            }
+          )
+        ]
+      ),
+      body: Form(
+        key: _formKey,
+        child: ListView(
+          padding: EdgeInsets.all(16.0),
+          children: <Widget>[
+            TextFormField(
+              decoration: InputDecoration(
+                hintText: 'E-mail'
+              ),
+              keyboardType: TextInputType.emailAddress,
+              validator: (text) {
+                if (text.isEmpty || !text.contains('@')) return 'E-mail inválido';
+              }
+            ),
+            SizedBox(height: 16.0),
+            TextFormField(
+              decoration: InputDecoration(
+                hintText: 'Senha'
+              ),
+              obscureText: true, // Para que o usuário não consiga ver a senha que está sendo digitada,
+              validator: (text) {
+                if (text.isEmpty || text.length < 8) return 'Senha inválida! A senha precisa ter pelo menos 8 caracteres!';
+              }
+            ),
+            Align(
+              alignment: Alignment.centerRight,
+              child: FlatButton(
+                onPressed: () {
+                  print('Esqueci minha senha');
+                },
+                child: Text('Esqueci minha senha',
+                  textAlign: TextAlign.right,
+                  style: TextStyle(fontWeight: FontWeight.w400, color: Colors.grey[600]),
+                ),
+                padding: EdgeInsets.zero
+              )
+            ),
+            SizedBox(height: 16.0),
+            SizedBox(
+              height: 44.0,
+              child: RaisedButton(
+                child: Text('Entrar',
+                  style: TextStyle(fontSize: 18.0)
+                ),
+                color: Theme.of(context).primaryColor,
+                textColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(4.0),
+                  side: BorderSide(color: Colors.teal[800])
+                ),
+                onPressed: () {
+                  if (_formKey.currentState.validate()) {
+
+                  }
+                }
+              )
+            )
+          ],
+        )
+      )
+    );
+  }
+}
