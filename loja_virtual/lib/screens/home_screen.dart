fc0a0a14638913e5ff6b4a682451eea890baf7b8
Lucas Monteiro <lukasmonteir0@hotmail.com>
2021-03-12 19:10:29 -0300

    Loja Virtual: Estruturando as lojas

--- loja_virtual/lib/screens/home_screen.dart
+++ loja_virtual/lib/screens/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:loja_virtual/tabs/home_tab.dart';
 import 'package:loja_virtual/tabs/orders_tab.dart';
+import 'package:loja_virtual/tabs/places_tab.dart';
 import 'package:loja_virtual/tabs/products_tab.dart';
 import 'package:loja_virtual/widgets/cart_button.dart';
 import 'package:loja_virtual/widgets/custom_drawer.dart';
@@ -30,7 +31,11 @@ class HomeScreen extends StatelessWidget {
           body: ProductsTab(),
           floatingActionButton: CartButton()
         ),
-        Container(color: Colors.green),
+        Scaffold(
+          appBar: AppBar(title: Text('Lojas'), centerTitle: true),
+          body: PlacesTab(),
+          drawer: CustomDrawer(_pageController)
+        ),
         Scaffold(
           appBar: AppBar(
             title: Text('Meus Pedidos'),
