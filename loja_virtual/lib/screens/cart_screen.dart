a1c95c0ca555690e0e2e9ba5708935842ab01b97
Lucas Monteiro <lukasmonteir0@hotmail.com>
2021-03-12 09:16:21 -0300

    Loja Virtual: Criando o resumo do pedido

--- loja_virtual/lib/screens/cart_screen.dart
+++ loja_virtual/lib/screens/cart_screen.dart
@@ -4,6 +4,7 @@ import 'package:loja_virtual/models/cart_model.dart';
 import 'package:loja_virtual/models/user_model.dart';
 import 'package:loja_virtual/screens/login_screen.dart';
 import 'package:loja_virtual/tiles/cart_tile.dart';
+import 'package:loja_virtual/widgets/cart_price.dart';
 import 'package:loja_virtual/widgets/discount_cart.dart';
 import 'package:loja_virtual/widgets/ship_card.dart';
 import 'package:scoped_model/scoped_model.dart';
@@ -86,7 +87,8 @@ class CartScreen extends StatelessWidget {
                   ).toList()
                 ),
                 DiscountCart(),
-                ShipCard()
+                ShipCard(),
+                CartPrice(() {})
               ],
             );
           }
