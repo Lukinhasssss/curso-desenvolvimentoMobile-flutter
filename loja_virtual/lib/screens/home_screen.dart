cddfa45a4300588102001c22695a7afcd9da8cc0
Lucas Monteiro <lukasmonteir0@hotmail.com>
2021-03-12 11:35:49 -0300

    Loja Virtual: Criando a Tab de pedidos

--- loja_virtual/lib/screens/home_screen.dart
+++ loja_virtual/lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:loja_virtual/tabs/home_tab.dart';
+import 'package:loja_virtual/tabs/orders_tab.dart';
 import 'package:loja_virtual/tabs/products_tab.dart';
 import 'package:loja_virtual/widgets/cart_button.dart';
 import 'package:loja_virtual/widgets/custom_drawer.dart';
@@ -30,7 +31,14 @@ class HomeScreen extends StatelessWidget {
           floatingActionButton: CartButton()
         ),
         Container(color: Colors.green),
-        Container(color: Colors.blue)
+        Scaffold(
+          appBar: AppBar(
+            title: Text('Meus Pedidos'),
+            centerTitle: true
+          ),
+          body: OrdersTab(),
+          drawer: CustomDrawer(_pageController),
+        )
       ], // children -> Receba vários widgets
     );
   }
